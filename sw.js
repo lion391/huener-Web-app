55ec734259f4a085b900b466bd554f0564d5ae11
lion391 <81109303+lion391@users.noreply.github.com>
2026-03-23 14:21:15 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'huehnerapp-v1';
+const ASSETS = [
+  './huehnerapp.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./huehnerapp.html')))
+  );
+});
